f328bd736e7e3a09c1d34b53c2e3fc82d2eccd50
Samuel <samuleal007@gmail.com>
2024-11-24 15:25:01 -0300

    Arrumado4

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cookieParser = require("cookie-parser");
 
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     res.redirect("/login");
